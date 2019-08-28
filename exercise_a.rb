469d2216ed3c4c02b1f05fea8ddd4d2184b42ca4
cjwales <cjwales@gmail.com>
2019-08-28 16:43:40 +0100

    almost finished b

---
+++ exercise_a.rb
@@ -0,0 +1,36 @@
+stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
+
+#Add "Edinburgh Waverley" to the end of the array
+stops.push("Edinburgh Waverley")
+
+ #Add "Glasgow Queen St" to the start of the array
+stops.unshift("Glasgow Queen St")
+
+#Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
+stops.insert(4, "Polmont")
+
+#Work out the index position of "Linlithgow"
+stops.find_index("Linlithgow")
+
+#Remove "Livingston" from the array using its name
+stops.delete("Livingston")
+
+#Delete "Cumbernauld" from the array by index
+stops.delete_at(2)
+
+#How many stops there are in the array?
+stops.count
+
+#How many ways can we return "Falkirk High" from the array?
+stops[2]
+stops.at(2)
+
+#Reverse the positions of the stops in the array
+stops.reverse
+
+#Print out all the stops using a for loop
+for station in stops
+  p station
+end
+
+#p stops
---
+++ exercise_b.rb
@@ -0,0 +1,88 @@
+users = {
+  "Jonathan" => {
+    :twitter => "jonnyt",
+    :lottery_numbers => [6, 12, 49, 33, 45, 20],
+    :home_town => "Stirling",
+    :pets => [
+    {
+      :name => "fluffy",
+      :species => "cat"
+    },
+    {
+      :name => "fido",
+      :species => "dog"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    }
+  ]
+  },
+  "Erik" => {
+    :twitter => "eriksf",
+    :lottery_numbers => [18, 34, 8, 11, 24],
+    :home_town => "Linlithgow",
+    :pets => [
+    {
+      :name => "nemo",
+      :species => "fish"
+    },
+    {
+      :name => "kevin",
+      :species => "fish"
+    },
+    {
+      :name => "spike",
+      :species => "dog"
+    },
+    {
+      :name => "rupert",
+      :species => "parrot"
+    }
+  ]
+  },
+  "Avril" => {
+    :twitter => "bridgpally",
+    :lottery_numbers => [12, 14, 33, 38, 9, 25],
+    :home_town => "Dunbar",
+    :pets => [
+      {
+        :name => "monty",
+        :species => "snake"
+      }
+    ]
+  }
+}
+
+#Get Jonathan's Twitter handle (i.e. the string "jonnyt")
+users ["Jonathan"][:twitter]
+
+#Get Erik's hometown
+users ["Jonathan"][:home_town]
+
+#Get the array of Erik's lottery numbers
+users ["Erik"][:lottery_numbers]
+
+#Get the type of Avril's pet Monty
+users ["Avril"][:pets][0][:species]
+
+#Get the smallest of Erik's lottery numbers
+#users ["Erik"][:lottery_numbers][2]
+users ["Erik"][:lottery_numbers].min()
+
+#Return an array of Avril's lottery numbers that are even
+even = []
+for number in users ["Avril"][:lottery_numbers]
+  if number.even?()
+    even.push(number)
+  end
+end
+
+#Erik is one lottery number short! Add the number 7 to be included in his lottery numbers
+users ["Erik"][:lottery_numbers].push(7)
+
+#Change Erik's hometown to Edinburgh
+users ["Erik"][:home_town].replace("Edinburgh")
+
+#Add a pet dog to Erik called "Fluffy"
+p users ["Erik"][:pets]
